c25d754f77b0aa473cc487c5d57bd8b03f2fc5d1
TaiMingLu <terrytaimingl@gmail.com>
2025-11-22 02:09:48 -0500

    update orbax lm

--- lm-evaluation-harness/scripts/test_orbax_eval.sh
+++ lm-evaluation-harness/scripts/test_orbax_eval.sh
@@ -162,7 +162,7 @@ for parent_dir in distill_pretrain pretrain; do
         --hf_model_path=${HF_MODEL_PATH} \
         --add_special_tokens=False \
         --eval_save_dir=${EVAL_RESULTS_DIR} \
-        --ppl_batch_size=4 \
+        --ppl_batch_size=2 \
         --acc_batch_size=64 \
     "
 
