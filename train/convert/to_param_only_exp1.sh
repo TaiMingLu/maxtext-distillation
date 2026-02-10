2664b52c982ae1336cab2668ca93c2de353499bf
TaiMingLu <terrytaimingl@gmail.com>
2026-02-09 22:00:23 -0500

    add bucket name

---
+++ train/convert/to_param_only_exp1.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+cd ~/maxtext
+
+echo "========================"
+echo "environment variables:"
+echo "TPU_PREFIX: $TPU_PREFIX"
+echo "BUCKET_NAME: $BUCKET_NAME"
+echo "========================"
+
+required_vars=(
+    "BUCKET_NAME"
+    "TPU_PREFIX"
+)
+for var in "${required_vars[@]}"; do
+  if [[ -z "${!var:-}" ]]; then
+    echo "[ERROR] $var is not set"
+    exit 1
+  fi
+done
+
+
+for run_dir in $(gsutil ls -d gs://${BUCKET_NAME}/ckpts/exp1/*/); do
+  run_name=$(basename "$run_dir")
+  echo "========================================"
+  echo "Processing: $run_name"
+  echo "========================================"
+
+  python -u multihost_runner_orig.py \
+      --TPU_PREFIX=$TPU_PREFIX \
+      --INTERNAL_IP=true \
+      --COMMAND="
+      export TPU_LOG_DIR=/home/terry/tpu_logs
+      source ~/maxtext_env/bin/activate
+      python3.10 -u -m MaxText.generate_param_only_checkpoint MaxText/configs/base.yml \
+        load_full_state_path=gs://${BUCKET_NAME}/ckpts/exp1/${run_name}/checkpoints/24999/items \
+        checkpoint_dir=gs://${BUCKET_NAME}/ckpts/exp1_param_only/${run_name}/checkpoints/24999/items \
+        enable_checkpointing=True async_checkpointing=False \
+        model_name=llama3.1-1b
+      "
+
+  if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to convert: $run_name. Continuing..."
+  else
+    echo "[SUCCESS] Converted: $run_name"
+  fi
+done
