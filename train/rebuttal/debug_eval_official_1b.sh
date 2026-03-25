b491dc965fdc6004ca726e6022ef46b2511373d8
TaiMingLu <terrytaimingl@gmail.com>
2026-03-24 22:44:05 -0400

    fix: remove duplicate attention=dot_product (already in model YAML)

--- train/rebuttal/debug_eval_official_1b.sh
+++ train/rebuttal/debug_eval_official_1b.sh
@@ -18,7 +18,6 @@ python3.10 -u scripts/test_orbax_eval.py ../MaxText/configs/base.yml \
     max_target_length=4096 \
     dtype=bfloat16 \
     scan_layers=true \
-    attention=dot_product \
     --hf_model_path="/home/terry/gcs-bucket/rebuttal/hf_models/Llama-3.1-8B" \
     --eval_mode=ppl \
     --eval_save_dir="/home/terry/gcs-bucket/rebuttal/debug_official_1b" \
